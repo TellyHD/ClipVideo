2c1da79bf2aee256fa57400f6da8c2610abdd519
ios_peng <peng_ios@sina.com>
2018-01-04 10:24:13 +0800

    init project

---
+++ ClipVideo/ClipVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClipVideo
+//
+//  Created by apple on 2018/1/4.
+//  Copyright © 2018年 Telly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
